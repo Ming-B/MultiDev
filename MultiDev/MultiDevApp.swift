e84f1e14b658b15b3f44b274c76452e2d4ccd064
Brian Krupp <briankrupp@icloud.com>
2025-02-24 12:03:01 -0500

    Initial Commit

---
+++ MultiDev/MultiDevApp.swift
@@ -0,0 +1,17 @@
+//
+//  MultiDevApp.swift
+//  MultiDev
+//
+//  Created by Brian Krupp on 2/24/25.
+//
+
+import SwiftUI
+
+@main
+struct MultiDevApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
